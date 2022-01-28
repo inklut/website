fefa46ba4c72467a36c70e5505ec9ff1d34b6808
Gary R Avendano R <10035313-inklut@users.noreply.gitlab.com>
2022-01-28 09:43:07 -0500

    error 404

--- main.py
+++ main.py
@@ -9,5 +9,11 @@ def index():
     title='Inklut'
     return render_template('frontend.html', title=title, now=datetime.utcnow())
 
+# Error 404
+@app.route('/<error>')
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('components/errors/404.html', error=error), 404
+
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
