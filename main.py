b44381d6479e4d3e5b0cdf8636c13368c02fb68f
Gary R Avendano R <webfils@hotmail.com>
2022-01-27 21:11:52 -0500

    paginas

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return render_template('home.html')
-
+    title='Inklut'
+    return render_template('frontend.html', title=title)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=1234))
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
