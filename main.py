e3ce656a9b126ff232affee09eb8cab17f7c6849
Gary R Avendano R <10035313-inklut@users.noreply.gitlab.com>
2022-01-28 10:01:39 -0500

    menu

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ def index():
     title='Inklut'
     return render_template('frontend.html', title=title, now=datetime.utcnow())
 
+@app.route('/multimedia')
+def multimedia():
+    return render_template('multimedia.html', now=datetime.utcnow())
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html', now=datetime.utcnow())
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html', now=datetime.utcnow())
+
 # Error 404
 @app.route('/<error>')
 @app.errorhandler(404)
