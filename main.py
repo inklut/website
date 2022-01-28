abe5af07879959cc9156f3cce268487b7f792047
Gary R Avendano R <10035313-inklut@users.noreply.gitlab.com>
2022-01-28 09:09:26 -0500

    footer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from datetime import datetime
 import os
 
 app = Flask(__name__)
@@ -6,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     title='Inklut'
-    return render_template('frontend.html', title=title)
+    return render_template('frontend.html', title=title, now=datetime.utcnow())
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
